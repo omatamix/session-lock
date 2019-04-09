2294e9155651c7f815a3ef62a7a80a37621aa884
Kooser6 <48822466+Kooser6@users.noreply.github.com>
2019-04-09 03:52:04 -0500

    Add exception interface.

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -1,1 +1,18 @@
+<?php
+declare(strict_types=1);
+/**
+ * Kooser Session - Securely manage and preserve session data.
+ * 
+ * @package Kooser\Session.
+ */
 
+namespace Kooser\Session\Exception;
+
+/**
+ * Ensures exceptions are throwable.
+ *
+ * @interface ExceptionInterface.
+ */
+interface ExceptionInterface extends \Throwable
+{
+}
