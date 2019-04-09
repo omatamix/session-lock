03df39faa4db5625e07314454c26240465493749
Kooser6 <48822466+Kooser6@users.noreply.github.com>
2019-04-09 04:11:53 -0500

    Add code.

--- src/SlimFrameworkMiddleware.php
+++ src/SlimFrameworkMiddleware.php
@@ -1,8 +1,38 @@
 <?php
-class ExampleMiddleware
+declare(strict_types=1);
+/**
+ * Kooser Session - Securely manage and preserve session data.
+ * 
+ * @package Kooser\Session.
+ */
+
+namespace Kooser\Session;
+
+/**
+ * Secure session management for Slim.
+ *
+ * This middleware ensures a secure session is running at all times.
+ *
+ * @class SlimFrameworkMiddleware.
+ */
+class SlimFrameworkMiddleware
 {
+
+    /** @var array $settings The session settings for Slim. */
+    private $settings = [];
+
     /**
-     * Example middleware invokable class
+     * Construct the Slim middleware.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct(array $settings)
+    {
+        $this->settings = $settings;
+    }
+
+    /**
+     * Kooser Session middleware invokable class
      *
      * @param  \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
      * @param  \Psr\Http\Message\ResponseInterface      $response PSR7 response
@@ -12,10 +42,10 @@ class ExampleMiddleware
      */
     public function __invoke($request, $response, $next)
     {
-        $response->getBody()->write('BEFORE');
+        if (!SessionManager::exists()) {
+            SessionManager::start($this->settings);
+        }
         $response = $next($request, $response);
-        $response->getBody()->write('AFTER');
-
         return $response;
     }
 }
