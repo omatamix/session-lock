fea008e44e40ac477b9c057b0c4fdaf804500275
Kooser6 <48822466+Kooser6@users.noreply.github.com>
2019-04-10 15:16:37 -0500

    Update SessionManagerTest.php

--- tests/SessionManagerTest.php
+++ tests/SessionManagerTest.php
@@ -1,1 +1,11 @@
+<?php
+use PHPUnit\Framework\TestCase;
 
+class SessionManagerTest extends TestCase
+{
+    public function testPushAndPop()
+    {
+        $sessionConfig = ['use_cookies' => \false];
+        Kooser\Session\SessionManager::start($sessionConfig);
+    }
+}
