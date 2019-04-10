14bc5505025d30baea64821bcbfee1114623aa1f
Kooser6 <48822466+Kooser6@users.noreply.github.com>
2019-04-10 15:59:30 -0500

    Update SessionManagerTest.php

--- tests/SessionManagerTest.php
+++ tests/SessionManagerTest.php
@@ -3,6 +3,9 @@ use PHPUnit\Framework\TestCase;
 
 class SessionManagerTest extends TestCase
 {
+    /**
+     * @runInSeparateProcess
+     */
     public function testSessionStart()
     {
         $sessionConfig = ['use_cookies' => \false];
