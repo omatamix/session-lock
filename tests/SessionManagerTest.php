40f1c3f6fa803d7bb103fd687985c66bfa894fcf
Kooser6 <48822466+Kooser6@users.noreply.github.com>
2019-04-10 16:30:22 -0500

    Update SessionManagerTest.php

--- tests/SessionManagerTest.php
+++ tests/SessionManagerTest.php
@@ -9,13 +9,16 @@ class SessionManagerTest extends TestCase
     public function testSessions()
     {
         $sessionConfig = ['use_cookies' => \false];
+        $handler = new Kooser\Session\Handler\FileSessionHandler();
+        Kooser\Session\SessionManager::setSaveHandler($handler, \true);
+        \session_save_path(\realpath(\dirname($_SERVER['DOCUMENT_ROOT']) . '/../session');
         $result = Kooser\Session\SessionManager::start($sessionConfig);
         $this->assertTrue($result);
         $id = Kooser\Session\SessionManager::id();
         $this->assertTrue(\is_string($id));
-        $id = "rmcotLLrooxkEOj";
+        $id = \session_create_id();
         Kooser\Session\SessionManager::id($id);
-        $id = Kooser\Session\SessionManager::id();
-        $this->assertTrue(($id == "rmcotLLrooxkEOj"));
+        $idS = Kooser\Session\SessionManager::id();
+        $this->assertTrue(($id == $idS));
     }
 }
