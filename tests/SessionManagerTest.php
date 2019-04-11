7468088303997f8ade6d396ffe3e487a46c1edf5
Kooser6 <48822466+Kooser6@users.noreply.github.com>
2019-04-10 23:27:28 -0500

    Fix errors.

--- tests/SessionManagerTest.php
+++ tests/SessionManagerTest.php
@@ -12,16 +12,14 @@ class SessionManagerTest extends TestCase
         $handler = new Kooser\Session\Handler\FileSessionHandler();
         Kooser\Session\SessionManager::setSaveHandler($handler, \true);
         \session_save_path(\realpath(\dirname(__DIR__ . '/session')));
-        $result = Kooser\Session\SessionManager::start($sessionConfig);
-        $this->assertTrue($result);
-        $id = Kooser\Session\SessionManager::id();
-        $this->assertTrue(\is_string($id));
-        $result = Kooser\Session\SessionManager::destroy();
-        $this->assertTrue($result);
         $id = \session_create_id();
         Kooser\Session\SessionManager::id($id);
         $idS = Kooser\Session\SessionManager::id();
         $this->assertTrue(($id == $idS));
+        $result = Kooser\Session\SessionManager::start($sessionConfig);
+        $this->assertTrue($result);
+        $id = Kooser\Session\SessionManager::id();
+        $this->assertTrue(\is_string($id));
         $result = Kooser\Session\SessionManager::start($sessionConfig);
         $this->assertTrue($result);
     }
