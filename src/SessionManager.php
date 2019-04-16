6cb7ffdc1e0273391216cf4ab8d27438af42c8d3
Kooser6 <48822466+Kooser6@users.noreply.github.com>
2019-04-15 23:36:45 -0500

    Change class name.

--- src/SessionManager.php
+++ src/SessionManager.php
@@ -42,7 +42,7 @@ final class SessionManager
             if (!isset($sessionConfig['fingerprint_validators'])) {
                $sessionConfig['fingerprint_validators'] = [];
             }
-            $fpManager = new FingerprintManager($sessionConfig['fingerprint_validators']);
+            $fpManager = new SessionFingerprintManager($sessionConfig['fingerprint_validators']);
             if (self::has("session_fingerprint")) {
                 $fp = $fpManager->generate();
                 if (!\hash_equals(self::get("session_fingerprint"), $fp)) {
