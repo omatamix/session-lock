610901926f59effc91e1e90a9128ded1b8f6de27
Nicholas <48822466+Kooser6@users.noreply.github.com>
2019-07-26 11:25:04 -0500

    Add session encrypter.

--- src/SessionEncrypted.php
+++ src/SessionEncrypted.php
@@ -1,1 +1,18 @@
+<?php
+declare(strict_types=1);
+/**
+ * Kooser Session - Securely manage and preserve session data.
+ * 
+ * @package Kooser\Session.
+ */
 
+namespace Kooser\Session;
+
+/**
+ * Session Encrypter
+ *
+ * @class SessionManager.
+ */
+class SessionEncrypted
+{
+}
