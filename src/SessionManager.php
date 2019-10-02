b09bfeb494639ac2e9fc91eaab459f0a3829e43f
Nicholas <48822466+Kooser6@users.noreply.github.com>
2019-10-02 03:04:47 -0400

    Inherit the documentation from the interface.

--- src/SessionManager.php
+++ src/SessionManager.php
@@ -28,12 +28,7 @@ final class SessionManager implements SessionManagerInterface
     private $exceptions;
 
     /**
-     * Construct a new session manager.
-     *
-     * @param array $options    The session manager options.
-     * @param bool  $exceptions Should we utilize exceptions.
-     *
-     * @return void Returns nothing.
+     * {@inheritdoc}
      */
     public function __construct(array $options = [], bool $exceptions = \true)
     {
@@ -42,11 +37,7 @@ final class SessionManager implements SessionManagerInterface
     }
 
     /**
-     * Set the session manager options.
-     *
-     * @param array $options The session manager options.
-     *
-     * @return \Kooser\Session\SessionManagerInterface Returns the session manager.
+     * {@inheritdoc}
      */
     public function setOptions(array $options = []): SessionManagerInterface
     {
@@ -57,11 +48,7 @@ final class SessionManager implements SessionManagerInterface
     }
 
     /**
-     * Set the exceptions param.
-     *
-     * @param bool $exceptions Should we utilize exceptions.
-     *
-     * @return \Kooser\Session\SessionManagerInterface Returns the session manager.
+     * {@inheritdoc}
      */
     public function setExceptions(bool $exceptions = \true): SessionManagerInterface
     {
@@ -70,12 +57,7 @@ final class SessionManager implements SessionManagerInterface
     }
 
     /**
-     * Starts or resumes a session.
-     *
-     * @throws Exception\InvalidFingerprintException If the fingerprint from the user does not match the one
-     *                                               binded to the session.
-     *
-     * @return bool Returns true on success or false on failure.
+     * {@inheritdoc}
      */
     public function start(): bool
     {
@@ -98,9 +80,7 @@ final class SessionManager implements SessionManagerInterface
     }
 
     /**
-     * Stop a session and destroys all data.
-     *
-     * @return bool Returns true on success or false on failure.
+     * {@inheritdoc}
      */
     public function stop(): bool
     {
@@ -121,9 +101,7 @@ final class SessionManager implements SessionManagerInterface
     }
 
     /**
-     * Check to see if a session already exists.
-     *
-     * @return bool Returns true if one exists and false if not.
+     * {@inheritdoc}
      *
      * @codeCoverageIgnore
      */
@@ -136,11 +114,7 @@ final class SessionManager implements SessionManagerInterface
     }
 
     /**
-     * Regenerates the session.
-     *
-     * @param bool $deleteOldSession Whether to delete the old session or not.
-     *
-     * @return bool Returns true on success or false on failure.
+     * {@inheritdoc}
      */
     public static function regenerate(bool $deleteOldSession = \true): bool
     {
@@ -148,11 +122,7 @@ final class SessionManager implements SessionManagerInterface
     }
 
     /**
-     * Checks whether a value for the specified key exists in the session.
-     *
-     * @param string $key The key to check.
-     *
-     * @return bool Returns true if the key was found and false if not.
+     * {@inheritdoc}
      */
     public function has(string $key): bool
     {
@@ -160,15 +130,7 @@ final class SessionManager implements SessionManagerInterface
     }
 
     /**
-     * Returns the requested value from the session or, if not found, the
-     * specified default value.
-     *
-     * @param string $key          The key to retrieve the value for.
-     * @param mixed  $defaultValue The default value to return if the
-     *                             requested value cannot be found.
-     *
-     * @return mixed Returns the requested value or the default
-     *               value.
+     * {@inheritdoc}
      */
     public static function get(string $key, $defaultValue = \null)
     {
@@ -179,16 +141,7 @@ final class SessionManager implements SessionManagerInterface
     }
 
     /**
-     * Returns the requested value and removes it from the session.
-     *
-     * This is identical to calling `get` first and then `remove` for the same
-     * key.
-     *
-     * @param string $key         The key to retrieve and remove the value for.
-     * @param mixed $defaultValue The default value to return if the requested
-     *                            value cannot be found.
-     *
-     * @return mixed The requested value or the default value.
+     * {@inheritdoc}
      */
     public static function flash(string $key, $defaultValue = \null)
     {
@@ -201,14 +154,7 @@ final class SessionManager implements SessionManagerInterface
     }
 
     /**
-     * Sets the value for the specified key to the given value.
-     *
-     * Any data that already exists for the specified key will be overwritten.
-     *
-     * @param string $key   The key to set the value for.
-     * @param mixed  $value The value to set.
-     *
-     * @return void Returns nothing.
+     * {@inheritdoc}
      */
     public static function put(string $key, $value): void
     {
@@ -216,11 +162,7 @@ final class SessionManager implements SessionManagerInterface
     }
 
     /**
-     * Removes the value for the specified key from the session.
-     *
-     * @param string $key The key to remove the value for.
-     *
-     * @return void Returns nothing.
+     * {@inheritdoc}
      */
     public static function delete(string $key): void
     {
