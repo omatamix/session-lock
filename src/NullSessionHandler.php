893abd91590fe215498aa223e962d91826bf4c2e
Nicholas <48822466+Kooser6@users.noreply.github.com>
2019-10-02 03:03:01 -0400

    Add null session handler.

--- src/NullSessionHandler.php
+++ src/NullSessionHandler.php
@@ -1,1 +1,66 @@
+<?php
+declare(strict_types=1);
+/**
+ * Kooser Session - Securely manage and preserve session data.
+ * 
+ * @package Kooser\Session.
+ */
 
+namespace Kooser\Session;
+
+use SessionHandlerInterface;
+
+/**
+ * A null session handler for testing.
+ */
+class NullSessionHandler implements SessionHandlerInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function open($savePath, $sessionName)
+    {
+        return \true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function close()
+    {
+        return \true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read($sessionId)
+    {
+        return '';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function write($sessionId, $data)
+    {
+        return \true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function destroy($sessionId)
+    {
+        return \true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function gc($lifetime)
+    {
+        return \true;
+    }
+}
