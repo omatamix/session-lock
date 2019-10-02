fdd46dc421bafd3b96e50d60ff2eb71985386947
Nicholas <48822466+Kooser6@users.noreply.github.com>
2019-10-02 16:30:15 -0400

    Add native session handler.

--- src/NativeSessionHandler.php
+++ src/NativeSessionHandler.php
@@ -1,1 +1,59 @@
+<?php
+declare(strict_types=1);
+/**
+ * Kooser Session - Securely manage and preserve session data.
+ * 
+ * @package Kooser\Session.
+ */
 
+namespace Kooser\Session;
+
+use SessionHandler;
+
+/**
+ * The native session handler.
+ *
+ * @class NativeSessionHandler.
+ *
+ * @codeCoverageIgnore
+ */
+class NativeSessionHandler extends SessionHandler
+{
+
+    /** @var StoreInterface $storeType The default store type. */
+    private $storeType;
+
+    /**
+     * Set the store object.
+     *
+     * @param StoreInterface $storeType The default store type.
+     *
+     * @return void Returns nothing.
+     */
+    public function setStore(StoreInterface $storeType): void
+    {
+        $this->storeType = $storeType;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read($id)
+    {
+        $data = parent::read($id);
+        if (!$data) {
+            return "";
+        } else {
+            return $this->storeType->decrypt($data);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function write($id, $data)
+    {
+        $data = $this->storeType->encrypt($data);
+        return parent::write($id, $data);
+    }
+}
