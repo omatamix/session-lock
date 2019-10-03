8793a85caaf0acbb8b1f2ffbad6fca390d8ce19e
Nicholas <48822466+Kooser6@users.noreply.github.com>
2019-10-03 17:12:34 -0400

    Add cache session handler.

--- src/CacheSessionHandler.php
+++ src/CacheSessionHandler.php
@@ -1,1 +1,110 @@
+<?php
+declare(strict_types=1);
+/**
+ * Kooser Session - Securely manage and preserve session data.
+ * 
+ * @package Kooser\Session.
+ */
+
+namespace Kooser\Session;
+
+use SessionHandlerInterface;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+
+/**
+ * A null session handler for testing.
+ *
+ * @class NullSessionHandler.
+ *
+ * @codeCoverageIgnore
+ */
+class CacheSessionHandler implements SessionHandlerInterface
+{
+
+    /** @var StoreInterface|null $storeType The default store type. */
+    private $storeType = \null;
+
+    /** @var \Symfony\Component\Cache\Adapter\AdapterInterface $cache The cache adapter. */
+    private $cache;
+
+    /**
+     * Construct the cache based session handler.
+     *
+     * @param \Symfony\Component\Cache\Adapter\AdapterInterface The cache adapter.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct(AdapterInterface $cacheAdapter)
+    {
+        $this->cache = $cacheAdapter;
+    }
+
+    /**
+     * Set the store object.
+     *
+     * @param StoreInterface $storeType The default store type.
+     *
+     * @return void Returns nothing.
+     */
+    public function setStore(StoreInterface $storeType): void
+    {
+        $this->storeType = $storeType;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function open($savePath, $sessionName)
+    {
+        return \true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function close()
+    {
+        return \true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read($sessionId)
+    {
+        $sessionImport = $this->cache->getItem($sessionId);
+        if (!$sessionImport->isHit()) {
+            return '';
+        }
+        return $sessionImport->get();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function write($sessionId, $data)
+    {
+        $sessionImport = $this->cache->getItem($sessionId);
+        $sessionImport->set($data);
+        $this->cache->save($sessionImport);
+        return \true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function destroy($sessionId)
+    {
+        $this->cache->delete($sessionId);
+        return \true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function gc($lifetime)
+    {
+        return \true;
+    }
+}
 
