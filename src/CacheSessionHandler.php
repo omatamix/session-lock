1ab204345223bb912693a8aae50cfd159110cb45
kooser6 <48822466+kooser6@users.noreply.github.com>
2020-01-27 16:27:10 -0500

    CS Fix.

--- src/CacheSessionHandler.php
+++ src/CacheSessionHandler.php
@@ -1,12 +1,31 @@
-<?php
-declare(strict_types=1);
+<?php declare(strict_types=1);
 /**
- * Kooser Session - Securely manage and preserve session data.
+ * Session - Securely manage and preserve session data.
+ *
+ * @license MIT License. (https://github.com/Commander-Ant-Screwbin-Games/session/blob/master/license)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  * 
- * @package Kooser\Session.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * https://github.com/Commander-Ant-Screwbin-Games/firecms/tree/master/src/Core
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @package Commander-Ant-Screwbin-Games/session.
  */
 
-namespace Kooser\Session;
+namespace Session;
 
 use SessionHandlerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
