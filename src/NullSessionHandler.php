dcc08845c6881eb4e9adb0a22d33a60d4e2b1370
omatamix <47761650+omatamix@users.noreply.github.com>
2020-08-09 15:35:56 -0400

    Enhance code.

--- src/NullSessionHandler.php
+++ src/NullSessionHandler.php
@@ -1,59 +1,14 @@
 <?php declare(strict_types=1);
-/**
- * Session - Securely manage and preserve session data.
- *
- * @license MIT License. (https://github.com/Commander-Ant-Screwbin-Games/session/blob/master/license)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * https://github.com/Commander-Ant-Screwbin-Games/firecms/tree/master/src/Core
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- * @package Commander-Ant-Screwbin-Games/session.
- */
 
-namespace Session;
+namespace Omatamix\SessionLock;
 
 use SessionHandlerInterface;
 
 /**
  * A null session handler for testing.
- *
- * @class NullSessionHandler.
- *
- * @codeCoverageIgnore
  */
 class NullSessionHandler implements SessionHandlerInterface
 {
-
-    /** @var StoreInterface|null $storeType The default store type. */
-    private $storeType = \null;
-
-    /**
-     * Set the store object.
-     *
-     * @param StoreInterface $storeType The default store type.
-     *
-     * @return void Returns nothing.
-     */
-    public function setStore(StoreInterface $storeType): void
-    {
-        $this->storeType = $storeType;
-    }
-
     /**
      * {@inheritdoc}
      */
