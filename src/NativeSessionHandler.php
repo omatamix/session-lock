79dee7be5be0d106171ff4c570e8e7b3575c9aec
omatamix <47761650+omatamix@users.noreply.github.com>
2020-08-15 02:58:23 -0400

    Add the native session handler.

--- src/NativeSessionHandler.php
+++ src/NativeSessionHandler.php
@@ -1,1 +1,50 @@
+<?php declare(strict_types=1);
 
+namespace Omatamix\SessionLock;
+
+use SessionHandler;
+
+class NativeSessionHandler extends SessionHandler
+{
+    /** @var \Omatamix\SessionLock\EncryptionWrapper $encryptionWrapper The encryption wrapper. */
+    private $encryptionWrapper;
+
+    /**
+     * Construct the native session handler.
+     *
+     * @param \Omatamix\SessionLock\EncryptionWrapper $encryptionWrapper The encryption wrapper.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct(EncryptionWrapper $encryptionWrapper = \null)
+    {
+        $this->encrpytionWrapper = $encryptionWrapper;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read($sessionId)
+    {
+        $data = parent::read($sessionId);
+        if (!$data) {
+            return '';
+        } else {
+            if (!\is_null($this->encryptionWrapper)) {
+                $data = $this->encryptionWrapper->encrypt($data);
+            }
+            return $data;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function write($sessionId, $data)
+    {
+        if (!\is_null($this->encryptionWrapper)) {
+            $data = $this->encryptionWrapper->encrypt($data);
+        }
+        return parent::write($sessionId, $data);
+    }
+}
