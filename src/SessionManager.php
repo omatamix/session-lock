602faf8df2d06f3ff9f95b123b23ade09ed20a73
kooser6 <48822466+kooser6@users.noreply.github.com>
2021-04-20 05:22:14 -0400

    Improve the code.

--- src/SessionManager.php
+++ src/SessionManager.php
@@ -2,6 +2,7 @@
 
 namespace Omatamix\SessionLock;
 
+use Omatamix\SessionLock\Exception\InvalidFingerprintException;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use SessionHandlerInterface;
 
@@ -13,15 +14,11 @@ final class SessionManager implements SessionManagerInterface
     /** @var array $options The session manager options. */
     private $options = [];
 
-    /** @var bool $exceptions Should we utilize exceptions. */
-    private $exceptions;
-
     /**
      * {@inheritdoc}
      */
-    public function __construct(array $options = [], bool $exceptions = \true)
+    public function __construct(array $options = [])
     {
-        $this->setExceptions($exceptions);
         $this->setOptions($options);
     }
 
@@ -39,35 +36,17 @@ final class SessionManager implements SessionManagerInterface
     /**
      * {@inheritdoc}
      */
-    public function setExceptions(bool $exceptions = \true): SessionManagerInterface
-    {
-        $this->exceptions = $exceptions;
-        return $this;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setSaveHandler(SessionHandlerInterface $sessionHandler): void
-    {
-        \session_set_save_handler($sessionHandler, \true);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function start(): bool
     {
-        $session = \session_start($this->options['session_config']);
-        if ($this->options['session_fingerprint']) {
+        $session = session_start($this->options['config']);
+        if ($this->options['fingerprinting']) {
             if ($this->has('session-lock.fingerprint')) {
-                $this->stop();
-                if ($this->exceptions) {
-                    throw new Exception\InvalidFingerprintException('The fingerprint supplied is invalid.');
+                if (!hash_equals($this->has('session-lock.fingerprint'), $this->getFingerprint())) {
+                    $this->stop();
+                    throw new InvalidFingerprintException('The fingerprint supplied is invalid.');
                 }
-                \trigger_error('The fingerprint supplied is invalid.', \E_USER_ERROR);
             } else {
-                $this->put('session-lock.fingerprint', 1);
+                $this->put('session-lock.fingerprint', $this->getFingerprint());
             }
         }
         return $session;
@@ -78,20 +57,16 @@ final class SessionManager implements SessionManagerInterface
      */
     public function stop(): bool
     {
-        $_SESSION = [];
-        if ($this->options['session_config']['use_cookies']) {
-            $params = \session_get_cookie_params();
-            \setcookie(
-                \session_name(),
-                '',
-                \time() - 42000,
-                $params['path'],
-                $params['domain'],
-                $params['secure'],
-                $params['httponly']
+        $_SESSION = array();
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"],
+                $params["samesite"]
             );
         }
-        return \session_destroy();
+        session_destroy();
     }
 
     /**
@@ -99,18 +74,18 @@ final class SessionManager implements SessionManagerInterface
      */
     public function exists(): bool
     {
-        if (\php_sapi_name() !== 'cli') {
-            return \session_status() === \PHP_SESSION_ACTIVE ? \true : \false;
+        if (php_sapi_name() !== 'cli') {
+            return session_status() === PHP_SESSION_ACTIVE ? true : false;
         }
-        return \false;
+        return false;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function regenerate(bool $deleteOldSession = \true): bool
+    public function regenerate(bool $deleteOldSession = true): bool
     {
-        return (bool) \session_regenerate_id($deleteOldSession);
+        return session_regenerate_id($deleteOldSession);
     }
 
     /**
@@ -124,22 +99,19 @@ final class SessionManager implements SessionManagerInterface
     /**
      * {@inheritdoc}
      */
-    public function get(string $key, $defaultValue = \null)
+    public function get(string $key, $defaultValue = null)
     {
-        return isset($_SESSION[$key]) ? $_SESSION[$key] : $defaultValue;
+        return $this->has($key) ? $_SESSION[$key] : $defaultValue;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function flash(string $key, $defaultValue = \null)
+    public function flash(string $key, $defaultValue = null)
     {
-        if (isset($_SESSION[$key])) {
-            $value = $_SESSION[$key];
-            unset($_SESSION[$key]);
-            return $value;
-        }
-        return $defaultValue;
+        $value = $this->get($key, $defaultValue);
+        unset($_SESSION[$key]);
+        return $value;
     }
 
     /**
@@ -167,15 +139,15 @@ final class SessionManager implements SessionManagerInterface
      */
     private function getFingerprint(): string
     {
-        $ip = $ua = '';
-        if ($this->options['session_lock_to_ip_address'] && isset($_SERVER['REMOTE_ADDR'])) {
-            $ip = !\is_null($this->options['session_pass_ip']) ? $this->options['session_pass_ip'] : $_SERVER['REMOTE_ADDR'];
+        $remoteIp = $userAgent = '';
+        if ($this->options['bind_ip_address'] && isset($_SERVER['REMOTE_ADDR'])) {
+            $remoteIp = !is_null($this->options['use_ip']) ? $this->options['use_ip'] : $_SERVER['REMOTE_ADDR'];
         }
-        if ($this->options['session_lock_to_user_agent'] && isset($_SERVER['HTTP_USER_AGENT'])) {
-            $ua = $_SERVER['HTTP_USER_AGENT'];
+        if ($this->options['bind_user_agent'] && isset($_SERVER['HTTP_USER_AGENT'])) {
+            $userAgent = $_SERVER['HTTP_USER_AGENT'];
         }
-        $rawFingerprint = \sprintf('%s|%s', $ip, $ua);
-        return \hash_hmac($this->options['session_fingerprint_hash'], $rawFingerprint, $this->options['session_security_code']);
+        $rawFingerprint = sprintf('%s|%s', $remoteIp, $userAgent);
+        return hash_hmac($this->options['fingerprint_hash'], $fingerprint, $this->options['security_code']);
     }
 
     /**
@@ -188,25 +160,25 @@ final class SessionManager implements SessionManagerInterface
     private function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'session_fingerprint'        => \true,
-            'session_fingerprint_hash'   => 'sha512',
-            'session_lock_to_ip_address' => \true,
-            'session_lock_to_user_agent' => \true,
-            'session_pass_ip' => \null,
-            'session_security_code' => '',
-            'session_config' => [
-                'use_cookies'      => \true,
-                'use_only_cookies' => \true,
-                'cookie_httponly'  => \true,
+            'fingerprinting'   => \true,
+            'fingerprint_hash' => 'sha512',
+            'bind_ip_address'  => true,
+            'bind_user_agent'  => true,
+            'use_ip'           => null,
+            'security_code'    => '',
+            'config'           => [
+                'use_cookies'      => true,
+                'use_only_cookies' => true,
+                'cookie_httponly'  => true,
                 'cookie_samesite'  => 'Lax',
-                'use_strict_mode'  => \true,
+                'use_strict_mode'  => true,
             ],
         ]);
-        $resolver->setAllowedTypes('session_fingerprint', 'bool');
-        $resolver->setAllowedTypes('session_fingerprint_hash', 'string');
-        $resolver->setAllowedTypes('session_lock_to_ip_address', 'bool');
-        $resolver->setAllowedTypes('session_lock_to_user_agent', 'bool');
-        $resolver->setAllowedTypes('session_config', 'array');
-        $resolver->setAllowedTypes('session_security_code', 'string');
+        $resolver->setAllowedTypes('fingerprint', 'bool');
+        $resolver->setAllowedTypes('fingerprint_hash', 'string');
+        $resolver->setAllowedTypes('bind_ip_address', 'bool');
+        $resolver->setAllowedTypes('bind_user_agent', 'bool');
+        $resolver->setAllowedTypes('security_code', 'string');
+        $resolver->setAllowedTypes('config', 'array');
     }
 }
